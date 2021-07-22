8e2ff6eeb6a3e76b6bf6d4d3df50009aac92356f
ShireenMeher <shireenmeher296@gmail.com>
2021-07-22 16:42:43 +0530

    problem 1 updated

--- day2/problem1.go
+++ day2/problem1.go
@@ -3,44 +3,68 @@ package main
 import (
 	"fmt"
 	"sort"
+	"sync"
 )
+
+type mymap struct{
+	mapp map[string]int
+	mtx sync.Mutex
+}
+
+func increament(mymap2 *mymap, a byte){
+	mymap2.mtx.Lock()
+	_,prs := mymap2.mapp[string(a)]
+
+	if prs{
+		mymap2.mapp[string(a)] = mymap2.mapp[string(a)]+1
+
+	}
+	if !prs {
+		mymap2.mapp[string(a)] = 1
+	}
+	mymap2.mtx.Unlock()
+}
 
 func main()  {
 	slc := make([]string,0)
-	var n int
-	fmt.Scanln(&n)
+	wg:= new(sync.WaitGroup)
 
+
+	var n int
+
+	fmt.Scanln(&n)
+	wg.Add(n)
 	for i:=0;i<n;i++{
 		var temp string
 		fmt.Scanln(&temp)
 		slc=append(slc, temp)
 	}
 
-	mapp:=make(map[string]int)
+	mymap2:=mymap{make(map[string]int),*new(sync.Mutex)}
 	for i:=0;i<n;i++{
-		go frequency(slc[i],mapp)
+		go frequency(slc[i],&mymap2,wg)
 	}
 
-	sorted_keys:=make([]string,len(mapp))
-	for k:= range mapp{
+	wg.Wait()
+
+	sorted_keys:=make([]string,0)
+	for k:= range mymap2.mapp{
 		sorted_keys=append(sorted_keys,k)
 	}
 	sort.Strings(sorted_keys)
 
 	for _, k := range sorted_keys {
-		fmt.Println(k," : ", mapp[k])
+		fmt.Println(k," : ", mymap2.mapp[k])
 	}
+
 }
 
-func frequency(str string, mapp map[string]int){
+func frequency(str string, mymap2 *mymap, wg *sync.WaitGroup){
 	var n int=len(str)
 
+	defer wg.Done()
 	for i:=0;i<n;i++{
-		_,prs := mapp[string(str[i])]
-		if prs{
-			mapp[string(str[i])] = mapp[string(str[i])]+1
-			continue
-		}
-		mapp[string(str[i])]=1
+		increament(mymap2,str[i])
+
 	}
 }
---
+++ day2/problem2.go
@@ -0,0 +1,3 @@
+package main
+
+func student_response
