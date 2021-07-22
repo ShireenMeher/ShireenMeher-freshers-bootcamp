1892996320d30ec172024496f79b2ee181adf260
ShireenMeher <shireenmeher296@gmail.com>
2021-07-22 17:26:49 +0530

    question 2 updated

--- day2/problem2.go
+++ day2/problem2.go
@@ -1,3 +1,36 @@
 package main
 
-func student_response
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var sum int =0
+
+func student_response(wg *sync.WaitGroup) {
+	defer wg.Done()
+	var rdm2 int
+	var rdm1 float32
+	rdm1=rand.Float32()
+	rdm2=rand.Intn(4)+1
+
+	time.Sleep(time.Duration(rdm1))
+	sum+=rdm2
+
+}
+
+func main(){
+	wg:= new(sync.WaitGroup)
+	wg.Add(200)
+
+	for i:=0;i<200;i++{
+		student_response(wg)
+	}
+
+	wg.Wait()
+	var avg_rating float64
+	avg_rating= float64(sum)/float64(200)
+	fmt.Println(avg_rating)
+}
